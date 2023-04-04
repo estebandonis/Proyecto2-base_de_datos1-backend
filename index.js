0e22e8622133949deb0737ecd638240eda0a5723
Esteban Donis <estebandonis10@gmail.com>
2023-04-04 13:23:10 -0600

    Avances1

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hola")
+const { Client } = require('pg')
+
+const obtenerMedicos = async () => {
+    const client = new Client({
+        host: "database-1.cdwyldhrs5op.us-east-1.rds.amazonaws.com",
+        user: "postgres",
+        port: 5432,
+        password: "proyecto123",
+        database: "proyecto2"
+    })
+    
+    await client.connect()
+    
+    const res = await client.query("SELECT * FROM medicos")
+    debugger
+    const result = res.rows 
+
+    await client.end()
+    return result
+}
+
+obtenerMedicos().then((result) => {
+    console.log(result)
+})
