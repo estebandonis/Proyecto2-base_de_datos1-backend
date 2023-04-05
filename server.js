9ca8a93f899d9f82cfc02cf856f19f043b77e4da
Esteban Donis <estebandonis10@gmail.com>
2023-04-04 21:56:48 -0600

    conexion a frontend

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express')
 const medicosRoutes = require('./src/medicos/routes')
 const app = express()
 const port = 3000
+const cors = require('cors')
 
-app.use(express.json())
+app.use(cors({
+    origin: '*'
+}))
 
 app.get("/", (req, res) => {
     res.send("hello world")
