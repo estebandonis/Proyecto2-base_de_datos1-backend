871cd7823c21bb5c55fa948abc108a1f6ae1f1bc
Esteban Donis <estebandonis10@gmail.com>
2023-04-09 20:47:28 -0600

    Update usuarios agregado

--- src/usuarios/queries.js
+++ src/usuarios/queries.js
@@ -1,11 +1,19 @@
 const getTipo = "SELECT tipo FROM usuarios WHERE correo = $1 AND contraseña = $2"
 const getByNum = "SELECT * FROM usuarios WHERE num_colegiado = $1"
-const getUsuarios = "SELECT * FROM Usuarios"
+const getUsuarios = "SELECT * FROM Usuarios ORDER BY userid ASC"
 const addUsuario = "INSERT INTO usuarios (tipo, correo, contraseña, num_colegiado) VALUES ('medico', $1, $2, $3)"
+const updateCorreo = "UPDATE usuarios SET correo = $1 WHERE num_colegiado = $2"
+const updateContraseña = "UPDATE usuarios SET contraseña = $1 WHERE num_colegiado = $2"
+const updateNum = "UPDATE usuarios SET numero_colegiado = $1 WHERE num_colegiado = $2"
+const updateLugarid = "UPDATE usuarios SET lugarid = $1 WHERE num_colegiado = $2"
 
 module.exports = {
     getTipo,
     getByNum,
     getUsuarios,
     addUsuario,
+    updateCorreo,
+    updateContraseña,
+    updateNum,
+    updateLugarid,
 }
--- src/usuarios/routes.js
+++ src/usuarios/routes.js
@@ -8,4 +8,9 @@ router.get("/", controller.getUsuarios)
 
 router.post("/:one&:two&:three", controller.addUsuarios)
 
+router.put("/update_correo/:one&:two", controller.updateCorreo)
+router.put("/update_password/:one&:two", controller.updateContraseña)
+router.put("/update_num/:one&:two", controller.updateNum)
+router.put("/update_lugarid/:one&:two", controller.updateLugarid)
+
 module.exports = router
