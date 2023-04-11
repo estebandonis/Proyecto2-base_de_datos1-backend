fc91c0d4830b2c0ccb0009f52ca4970ac9b13106
José Auyón <josea13p@gmail.com>
2023-04-10 19:32:02 -0600

    Edicion de pacientes

--- src/pacientes/queries.js
+++ src/pacientes/queries.js
@@ -2,8 +2,8 @@ const getPacientes = 'SELECT * FROM pacientes';
 const getPacienteById = "SELECT * FROM pacientes WHERE pacienteid = $1";
 const checkPacienteExists= "SELECT nombre FROM pacientes WHERE dpi = $1";
 const addPaciente = "INSERT INTO pacientes (nombre, apellido, telefono, direccion, masa_corporal, altura, peso, adicciones,evoluciones,status,dpi) VALUES ($1, $2, $3, $4, $5, $6, $7,$8,$9,$10,$11,$12)"
-const updatePaciente = "UPDATE pacientes SET nombre = $1 WHERE pacienteid = $2"
+const updatePacienteNombre= "UPDATE pacientes SET nombre = $1 WHERE pacienteid = $2"
 
 module.exports ={
-    getPacientes, getPacienteById, addPaciente, checkPacienteExists,
+    getPacientes, getPacienteById, addPaciente, checkPacienteExists, updatePacienteNombre,
 }
