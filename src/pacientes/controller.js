28badab37c50207d68d210ceac3028b8cde590f5
José Auyón <josea13p@gmail.com>
2023-04-11 12:07:41 -0600

    Modificaciones de pacientes

--- src/pacientes/controller.js
+++ src/pacientes/controller.js
@@ -14,9 +14,9 @@ const getPacientes = (req,res)=>{
     })
 }
 const getPacienteById = (req,res) => {
-    const id = parseInt(req.params.id)
+    const dpi = req.params.dpi
 
-    pool.query(queries.getPacienteById, [id], (error, results) => {
+    pool.query(queries. getPacienteByDpi, [dpi], (error, results) => {
         if (error) throw error
         res.status(200).json(results.rows)
     })
@@ -41,6 +41,176 @@ const addPaciente = (req, res) => {
     })
 }
 
+const updatePacienteNombre = (req, res) => {
+    const dpi = req.params.dpi
+    const nombre = req.params.nombre
+console.log(dpi)
+console.log(nombre)
+    pool.query(queries. getPacienteByDpi, [dpi], (error, results) => {
+        const noExistePaciente = !results.rows.length
+        if (noExistePaciente) {
+            res.send("El paciente no existe")
+        }
+        console.log("Pasó")
+        pool.query(queries.updatePacienteNombre, [nombre, pacienteid], (error, results) => {
+            if (error) throw error
+            res.status(200).send("Paciente actualizado exitosamente")
+        })
+    })
+}
+
+const updatePacienteApellido = (req, res) => {
+    const dpi = req.params.dpi
+    const apellido = req.params.apellido
+
+    pool.query(queries. getPacienteByDpi, [dpi], (error, results) => {
+        const noExistePaciente = !results.rows.length
+        if (noExistePaciente) {
+            res.send("El paciente no existe")
+        }
+
+        pool.query(queries.updatePacienteApellido, [apellido, dpi], (error, results) => {
+            if (error) throw error
+            res.status(200).send("Paciente actualizado exitosamente")
+        })
+    })
+}
+
+const updatePacienteTelefono = (req, res) => {
+    const dpi = req.params.dpi
+    const telefono = req.params.telefono
+
+    pool.query(queries.getPacienteByDpi, [dpi], (error, results) => {
+        const noExistePaciente = !results.rows.length
+        if (noExistePaciente) {
+            res.send("El paciente no existe")
+        }
+
+        pool.query(queries.getPacienteByDpi, [telefono, dpi], (error, results) => {
+            if (error) throw error
+            res.status(200).send("Paciente actualizado exitosamente")
+        })
+    })
+}
+const updatePacienteDireccion = (req, res) => {
+    const dpi = req.params.dpi
+    const direccion = req.params.direccion
+
+    pool.query(queries.getPacienteByDpi, [dpi], (error, results) => {
+        const noExistePaciente = !results.rows.length
+        if (noExistePaciente) {
+            res.send("El paciente no existe")
+        }
+
+        pool.query(queries.getPacienteByDpi, [direccion, dpi], (error, results) => {
+            if (error) throw error
+            res.status(200).send("Paciente actualizado exitosamente")
+        })
+    })
+}
+
+const updatePacienteMasa = (req, res) => {
+    const dpi = req.params.dpi
+    const masa = req.params.masa_corporal
+
+    pool.query(queries.getPacienteByDpi, [dpi], (error, results) => {
+        const noExistePaciente = !results.rows.length
+        if (noExistePaciente) {
+            res.send("El paciente no existe")
+        }
+
+        pool.query(queries.getPacienteByDpi, [masa, dpi], (error, results) => {
+            if (error) throw error
+            res.status(200).send("Paciente actualizado exitosamente")
+        })
+    })
+}
+
+const updatePacienteAltura= (req, res) => {
+    const dpi = req.params.dpi
+    const altura = req.params.altura
+
+    pool.query(queries.getPacienteByDpi, [dpi], (error, results) => {
+        const noExistePaciente = !results.rows.length
+        if (noExistePaciente) {
+            res.send("El paciente no existe")
+        }
+
+        pool.query(queries.getPacienteByDpi, [altura, dpi], (error, results) => {
+            if (error) throw error
+            res.status(200).send("Paciente actualizado exitosamente")
+        })
+    })
+}
+
+const updatePacientePeso= (req, res) => {
+    const dpi = req.params.dpi
+    const altura = req.params.peso
+
+    pool.query(queries.getPacienteByDpi, [dpi], (error, results) => {
+        const noExistePaciente = !results.rows.length
+        if (noExistePaciente) {
+            res.send("El paciente no existe")
+        }
+
+        pool.query(queries.getPacienteByDpi, [peso, dpi], (error, results) => {
+            if (error) throw error
+            res.status(200).send("Paciente actualizado exitosamente")
+        })
+    })
+}
+const updatePacienteAdicciones= (req, res) => {
+    const dpi = req.params.dpi
+    const adicciones = req.params.adicciones
+
+    pool.query(queries.getPacienteByDpi, [dpi], (error, results) => {
+        const noExistePaciente = !results.rows.length
+        if (noExistePaciente) {
+            res.send("El paciente no existe")
+        }
+
+        pool.query(queries.getPacienteByDpi, [adicciones, dpi], (error, results) => {
+            if (error) throw error
+            res.status(200).send("Paciente actualizado exitosamente")
+        })
+    })
+}
+const updatePacienteEvoluciones= (req, res) => {
+    const dpi = req.params.dpi
+    const evoluciones = req.params.evoluciones
+
+    pool.query(queries.getPacienteByDpi, [dpi], (error, results) => {
+        const noExistePaciente = !results.rows.length
+        if (noExistePaciente) {
+            res.send("El paciente no existe")
+        }
+
+        pool.query(queries.getPacienteByDpi, [evoluciones, dpi], (error, results) => {
+            if (error) throw error
+            res.status(200).send("Paciente actualizado exitosamente")
+        })
+    })
+}
+const updatePacienteEstatus= (req, res) => {
+    const dpi = req.params.dpi
+    const estatus = req.params.estatus
+
+    pool.query(queries.getPacienteByDpi, [dpi], (error, results) => {
+        const noExistePaciente = !results.rows.length
+        if (noExistePaciente) {
+            res.send("El paciente no existe")
+        }
+
+        pool.query(queries.getPacienteByDpi, [estatus, dpi], (error, results) => {
+            if (error) throw error
+            res.status(200).send("Paciente actualizado exitosamente")
+        })
+    })
+}
+
+
 module.exports ={
-    getPacientes, getPacienteById, addPaciente
+    getPacientes, getPacienteById, addPaciente, updatePacienteNombre, updatePacienteApellido,
+     updatePacienteTelefono, updatePacienteDireccion, updatePacienteMasa, updatePacienteAltura,
+      updatePacientePeso, updatePacienteAdicciones, updatePacienteEvoluciones, updatePacienteEstatus,
 }
--- src/pacientes/queries.js
+++ src/pacientes/queries.js
@@ -1,9 +1,20 @@
 const getPacientes = 'SELECT * FROM pacientes';
-const getPacienteById = "SELECT * FROM pacientes WHERE pacienteid = $1";
-const checkPacienteExists= "SELECT nombre FROM pacientes WHERE dpi = $1";
+const getPacienteByDpi= "SELECT * FROM pacientes WHERE dpi = $1";
+const checkPacienteExists= "SELECT nombre FROM pacientes WHERE dpi= $1";
 const addPaciente = "INSERT INTO pacientes (nombre, apellido, telefono, direccion, masa_corporal, altura, peso, adicciones,evoluciones,status,dpi) VALUES ($1, $2, $3, $4, $5, $6, $7,$8,$9,$10,$11,$12)"
-const updatePacienteNombre= "UPDATE pacientes SET nombre = $1 WHERE pacienteid = $2"
+const updatePacienteNombre= "UPDATE pacientes SET nombre = $1 WHERE dpi = $2";
+const updatePacienteApellido = "UPDATE pacientes SET apellido = $1 WHERE dpi = $2";
+const updatePacienteTelefono = "UPDATE pacientes SET telefono = $1 WHERE dpi = $2";
+const updatePacienteDireccion = "UPDATE pacientes SET direccion = $1 WHERE dpi = $2";
+const updatePacienteMasa = "UPDATE pacientes SET masa_corporal = $1 WHERE dpi = $2";
+const updatePacienteAltura = "UPDATE pacientes SET altura = $1 WHERE dpi = $2";
+const updatePacientePeso = "UPDATE pacientes SET peso= $1 WHERE dpi = $2";
+const updatePacienteAdicciones = "UPDATE pacientes SET adicciones = $1 WHERE dpi = $2";
+const updatePacientEvoluciones = "UPDATE pacientes SET evoluciones = $1 WHERE dpi = $2";
+const updatePacienteEstatus = "UPDATE pacientes SET status = $1 WHERE dpi = $2";
 
 module.exports ={
-    getPacientes, getPacienteById, addPaciente, checkPacienteExists, updatePacienteNombre,
+    getPacientes, getPacienteByDpi, addPaciente, checkPacienteExists, updatePacienteNombre, 
+    updatePacienteApellido, updatePacienteTelefono,updatePacienteDireccion,updatePacienteMasa,
+    updatePacienteAltura, updatePacientePeso, updatePacienteAdicciones, updatePacientEvoluciones, updatePacienteEstatus,
 }
